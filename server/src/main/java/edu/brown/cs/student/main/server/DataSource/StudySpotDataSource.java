d118c8d4f867fca3a941bf83a8d148f497d6bf32
jinholee17 <jinho_lee@brown.edu>
2024-04-23 00:08:14 -0400

    feat: location implemented in algorithm in backend

--- server/src/main/java/edu/brown/cs/student/main/server/DataSource/StudySpotDataSource.java
+++ server/src/main/java/edu/brown/cs/student/main/server/DataSource/StudySpotDataSource.java
@@ -11,6 +11,7 @@ import edu.brown.cs.student.main.server.enums.Volume;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.sql.SQLOutput;
 import java.util.Calendar;
 import java.nio.file.Paths;
 import java.util.List;
@@ -41,15 +42,16 @@ public class StudySpotDataSource {
    * @param capacity
    * @return
    */
-  public StudySpot match(Volume volume, Traffic traffic, Capacity capacity) {
+  public StudySpot match(Volume volume, Traffic traffic, Capacity capacity, Double lon, Double lat,
+      String accessible,String whiteboard) {
     int maxIndex = 0;
-    int maxScore = 0;
+    double maxScore = 0;
 
     // TODO: Account for current date and time
 
     for (int i = 0; i < this.parsed.size(); i++) {
       StudySpot s = this.parsed.get(i);
-      int score = 0;
+      double score = 0;
       if (volume != null && volume.equals(s.volume)) {
         score += 1;
       }
@@ -59,9 +61,22 @@ public class StudySpotDataSource {
       if (capacity != null && capacity.equals(s.capacity)) {
         score += 1;
       }
-      score += s.accessibility;
-      if (s.whiteboard) {
-        score += 1;
+      if (accessible != null)
+      {
+          if (accessible.equals("yes") || accessible.equals("true")) {
+            score += s.accessibility;
+          }
+      }
+      if (whiteboard != null) {
+        if (whiteboard.equals("yes") || whiteboard.equals("true")) {
+          if (s.whiteboard) {
+            score += 1;
+          }
+        }
+      }
+      // subtracts by 'how close' a study spot is by weight of 3*difference
+      if (lon != null && lat != null) {
+        score -= getClose(lon,lat,s.longitude,s.latitude);
       }
       if (s.time.equals(this.getTime())){
         score += 1;
@@ -71,7 +86,7 @@ public class StudySpotDataSource {
         maxIndex = i;
       }
     }
-
+    System.out.println(maxScore);
     return this.parsed.get(maxIndex);
   }
 
@@ -89,6 +104,22 @@ public class StudySpotDataSource {
       timePeriod = Time.EVENING;
     }
     return timePeriod;
+  }
+
+  /**
+   * Subtracts the difference of coordinates- can be adjusted
+   * based on how much you want location to be a weight in the
+   * algorithm. If a place is close by, it will return < 0.01.
+   * If a place is far, it will return > 0.4
+   * @param mylon
+   * @param mylat
+   * @param lon
+   * @param lat
+   * @return
+   */
+  public double getClose(Double mylon,Double mylat, Double lon, Double lat){
+    return Math.abs(3*(mylon - lon + (mylat - lat)));
+
   }
   public List<StudySpot> getParsed() {
     return this.parsed;
