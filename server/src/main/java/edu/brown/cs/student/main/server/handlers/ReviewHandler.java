697094552652b6779af14287992ae7af81447a2c
Hee Su Chung <jhschung88@gmail.com>
2024-04-29 19:41:04 -0400

    fix: checking front end feature selection

--- server/src/main/java/edu/brown/cs/student/main/server/handlers/ReviewHandler.java
+++ server/src/main/java/edu/brown/cs/student/main/server/handlers/ReviewHandler.java
@@ -4,27 +4,25 @@
 // import spark.Response;
 // import spark.Route;
 
-
 public class ReviewHandler implements Route {
-    @Override
-    public Object handle(Request request, Response response) {
-        Map<String, Object> responseMap = new HashMap<>();
-        // try {
-        // String uid = request.queryParams("uid");
+  @Override
+  public Object handle(Request request, Response response) {
+    Map<String, Object> responseMap = new HashMap<>();
+    // try {
+    // String uid = request.queryParams("uid");
 
-//     // // Remove the user from the database
-//     // System.out.println("clearing words for user: " + uid);
-//     // this.storageHandler.clearUser(uid);
+    //     // // Remove the user from the database
+    //     // System.out.println("clearing words for user: " + uid);
+    //     // this.storageHandler.clearUser(uid);
 
-//     // responseMap.put("response_type", "success");
-//     // } catch (Exception e) {
-//     // // error likely occurred in the storage handler
-//     // e.printStackTrace();
-//     // responseMap.put("response_type", "failure");
-//     // responseMap.put("error", e.getMessage());
-//     // }
+    //     // responseMap.put("response_type", "success");
+    //     // } catch (Exception e) {
+    //     // // error likely occurred in the storage handler
+    //     // e.printStackTrace();
+    //     // responseMap.put("response_type", "failure");
+    //     // responseMap.put("error", e.getMessage());
+    //     // }
 
-
-        return Utils.toMoshiJson(responseMap);
+    return Utils.toMoshiJson(responseMap);
   }
 }
